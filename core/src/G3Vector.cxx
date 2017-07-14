c52ef0c9552285c9f0c209d0bd73f17494b3340f
nlharr <nlharring@gmail.com>
2017-07-14 16:54:01 -0700

    Added complex vector bindings (#29)
    * Added complex vector bindings
    
    * Added a unit test for the complex conversion.  This lives in the todfilter code since it's mostly relevant for the complex vector type.  I could move it to core if you want

--- core/src/G3Vector.cxx
+++ core/src/G3Vector.cxx
@@ -2,9 +2,11 @@
 #include <serialization.h>
 #include <container_pybindings.h>
 #include <G3Vector.h>
+#include <complex>
 
 G3_SERIALIZABLE_CODE(G3VectorInt);
 G3_SERIALIZABLE_CODE(G3VectorDouble);
+G3_SERIALIZABLE_CODE(G3VectorComplexDouble);
 G3_SERIALIZABLE_CODE(G3VectorString);
 G3_SERIALIZABLE_CODE(G3VectorFrameObject);
 G3_SERIALIZABLE_CODE(G3VectorUnsignedChar);
@@ -85,6 +87,57 @@ container_from_object(boost::python::object v)
 	return x;
 }
 
+template <>
+G3VectorComplexDoublePtr
+container_from_object(boost::python::object v)
+{
+	G3VectorComplexDoublePtr x(new (G3VectorComplexDouble));
+	Py_buffer view;
+	if (PyObject_GetBuffer(v.ptr(), &view,
+	    PyBUF_FORMAT | PyBUF_ANY_CONTIGUOUS) != -1) {
+		if (strcmp(view.format, "Zd") == 0) {
+			x->insert(x->begin(), (std::complex<double> *)view.buf,
+		       (std::complex<double> *)view.buf + view.len/sizeof(std::complex<double>));
+		} else if (strcmp(view.format, "Zf") == 0) {
+			x->resize(view.len/sizeof(std::complex<float>));
+			for (size_t i = 0; i < view.len/sizeof(std::complex<float>); i++)
+				(*x)[i] = ((std::complex<float> *)view.buf)[i];
+		} else if (strcmp(view.format, "d") == 0) {
+			x->resize(view.len/sizeof(double));
+			for (size_t i = 0; i < view.len/sizeof(double); i++)
+				(*x)[i] = ((double *)view.buf)[i];
+		} else if (strcmp(view.format, "f") == 0) {
+			x->resize(view.len/sizeof(float));
+			for (size_t i = 0; i < view.len/sizeof(float); i++)
+				(*x)[i] = ((float *)view.buf)[i];
+		} else if (strcmp(view.format, "i") == 0) {
+			x->resize(view.len/sizeof(int));
+			for (size_t i = 0; i < view.len/sizeof(int); i++)
+				(*x)[i] = ((int *)view.buf)[i];
+		} else if (strcmp(view.format, "I") == 0) {
+			x->resize(view.len/sizeof(int));
+			for (size_t i = 0; i < view.len/sizeof(int); i++)
+				(*x)[i] = ((unsigned int *)view.buf)[i];
+		} else if (strcmp(view.format, "l") == 0) {
+			x->resize(view.len/sizeof(long));
+			for (size_t i = 0; i < view.len/sizeof(long); i++)
+				(*x)[i] = ((unsigned long *)view.buf)[i];
+		} else {
+			// We could add more types, but why do that?
+			// Let Python do the work for obscure cases
+			boost::python::container_utils::extend_container(*x, v);
+		}
+		PyBuffer_Release(&view);
+	} else {
+		PyErr_Clear();
+		boost::python::container_utils::extend_container(*x, v);
+	}
+
+	return x;
+}
+
+
+
 static int
 G3VectorDouble_getbuffer(PyObject *obj, Py_buffer *view, int flags)
 {
@@ -127,6 +180,54 @@ G3VectorDouble_getbuffer(PyObject *obj, Py_buffer *view, int flags)
 
 	return 0;
 }
+
+
+
+static int
+G3VectorComplexDouble_getbuffer(PyObject *obj, Py_buffer *view, int flags)
+{
+	if (view == NULL) {
+		PyErr_SetString(PyExc_ValueError, "NULL view");
+		return -1;
+	}
+
+	view->shape = NULL;
+
+	boost::python::handle<> self(boost::python::borrowed(obj));
+	boost::python::object selfobj(self);
+	G3VectorComplexDoublePtr ts = 
+	    boost::python::extract<G3VectorComplexDoublePtr>(selfobj)();
+	view->obj = obj;
+	view->buf = (void*)&(*ts)[0];
+	view->len = ts->size() * sizeof(std::complex<double> );
+	view->readonly = 0;
+	view->itemsize = sizeof(std::complex<double> );
+	if (flags & PyBUF_FORMAT)
+		view->format = (char *)"Zd";
+	else
+		view->format = NULL;
+	view->ndim = 1;
+#if (PY_MAJOR_VERSION == 2 && PY_MINOR_VERSION < 7) || (PY_MAJOR_VERSION >= 3)
+	// Abuse internal pointer in the absence of smalltable. This is safe
+	// on all architectures except MIPS N32.
+	view->internal = (void *)ts->size();
+	view->shape = (Py_ssize_t *)(&view->internal);
+#else
+	view->smalltable[0] = ts->size();
+	view->shape = &view->smalltable[0];
+	view->internal = NULL;
+#endif
+	view->strides = &view->itemsize;
+	view->suboffsets = NULL;
+
+	// Try to hold onto our collective hats. This is still very dangerous if
+	// the vector's underlying vector is resized.
+	Py_INCREF(obj);
+
+	return 0;
+}
+
+
 
 static int
 G3VectorInt_getbuffer(PyObject *obj, Py_buffer *view, int flags)
@@ -172,6 +273,7 @@ G3VectorInt_getbuffer(PyObject *obj, Py_buffer *view, int flags)
 }
 
 static PyBufferProcs vecdouble_bufferprocs;
+static PyBufferProcs veccomplexdouble_bufferprocs;
 static PyBufferProcs vecint_bufferprocs;
 
 template<>
@@ -212,6 +314,18 @@ PYBINDINGS("core") {
 #if PY_MAJOR_VERSION < 3
 	vdclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER;
 #endif
+	boost::python::object veccomplexdouble = register_g3vector<std::complex<double> >(
+	    "G3VectorComplexDouble", "Array of complex floats. Treat as a serializable "
+	    "version of numpy.array(dtype=complex128). Can be efficiently cast "
+	    "to and from numpy arrays.");
+	// Add buffer protocol interface
+	PyTypeObject *vcclass = (PyTypeObject *)veccomplexdouble.ptr();
+	veccomplexdouble_bufferprocs.bf_getbuffer = G3VectorComplexDouble_getbuffer,
+	vcclass->tp_as_buffer = &veccomplexdouble_bufferprocs;
+#if PY_MAJOR_VERSION < 3
+	vcclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER;
+#endif
+
 	boost::python::object vecint = register_g3vector<int32_t>("G3VectorInt",
 	    "Array of integers. Treat as a serializable version of "
 	    "numpy.array(dtype=int32). Can be efficiently cast to and from "
@@ -224,8 +338,6 @@ PYBINDINGS("core") {
 	viclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER;
 #endif
 
-	register_g3vector<std::complex<double> >("G3VectorComplexDouble",
-	    "Array of complex numbers.");
 	register_g3vector<std::string>("G3VectorString", "List of strings.");
 	register_g3vector<G3FrameObjectPtr>("G3VectorFrameObject", "List of "
 	    "generic frame objects. Can lead to paradoxes; avoid use of this "
