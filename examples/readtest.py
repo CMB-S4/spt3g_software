080813f2b400c35db28a9a40c9e503477d90ac35
Nathan Whitehorn <nwhitehorn@physics.ucla.edu>
2017-07-21 14:48:57 -0700

    Clean up example code a little to avoid reading a file from /tmp and describe what it does.

--- examples/readtest.py
+++ examples/readtest.py
@@ -1,11 +1,14 @@
 from spt3g import core
-import time
+import time, sys
+
+# Read in one or more files specified at the command line and print their
+# contents to the console.
 
 pipe = core.G3Pipeline()
 
-pipe.Add(core.G3Reader('/tmp/test2.g3'))
+pipe.Add(core.G3Reader, filename=sys.argv[1:])
 def printfr(fr):
 	print(fr)
-pipe.Add(printfr)
+pipe.Add(printfr) # core.Dump does the same thing as this
 
 pipe.Run()
