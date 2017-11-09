91f2d18caedc5164dd6f15f9ad5850203a1588d0
Sasha Rahlin <arahlin@fnal.gov>
2017-11-09 12:28:15 -0600

    not all G3FrameObject subclasses have a value attribute

--- core/python/dataextensions.py
+++ core/python/dataextensions.py
@@ -4,15 +4,15 @@ __all__ = []
 
 def get_value(obj):
     if isinstance(obj, G3FrameObject):
-        return obj.value
+        return getattr(obj, 'value', obj)
     return obj
 
 def g3data_comparison_wrap(cls):
-    cls.__eq__ = lambda a, b: a.value == get_value(b)
-    cls.__neq__ = lambda a, b: a.value != get_value(b)
-    cls.__ge__ = lambda a, b: a.value >= get_value(b)
-    cls.__gt__ = lambda a, b: a.value > get_value(b)
-    cls.__le__ = lambda a, b: a.value <= get_value(b)
-    cls.__lt__ = lambda a, b: a.value < get_value(b)
+    cls.__eq__ = lambda a, b: get_value(a) == get_value(b)
+    cls.__neq__ = lambda a, b: get_value(a) != get_value(b)
+    cls.__ge__ = lambda a, b: get_value(a) >= get_value(b)
+    cls.__gt__ = lambda a, b: get_value(a) > get_value(b)
+    cls.__le__ = lambda a, b: get_value(a) <= get_value(b)
+    cls.__lt__ = lambda a, b: get_value(a) < get_value(b)
 
 g3data_comparison_wrap(G3FrameObject)
