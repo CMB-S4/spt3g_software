1b68bb46eaa664588ef64b0a37d36ab23a958b55
Sasha Rahlin <arahlin@fnal.gov>
2018-11-18 20:33:12 -0600

    tabs -> spaces

--- dfmux/bin/ledgerman.py
+++ dfmux/bin/ledgerman.py
@@ -9,22 +9,22 @@ parser.add_argument('hardware_map', metavar='/path/to/hwm.yaml', help='Path to h
 parser.add_argument('output', metavar='output.nc', help='Path to output NetCDF file')
 
 parser.add_argument('-v', dest='verbose', action='store_true',
-		    help='Verbose mode (print all frames)')
+                    help='Verbose mode (print all frames)')
 parser.add_argument('-a', dest='align', action='store_true',
-		    help='Align sampling. This has to happen once, but'
-		    ' will break any existing DAN loops when run.')
+                    help='Align sampling. This has to happen once, but'
+                    ' will break any existing DAN loops when run.')
 parser.add_argument('-s', dest='system_time', action='store_true',
-		    help='Replace board time with system time when data'
-		    ' received. Useful if your board is in IRIG_TEST mode.')
+                    help='Replace board time with system time when data'
+                    ' received. Useful if your board is in IRIG_TEST mode.')
 parser.add_argument('-P', dest='physnames', action='store_true',
-		    help='Use physical bolometer names rather than channel names')
+                    help='Use physical bolometer names rather than channel names')
 parser.add_argument('-p', dest='pathstring', action='store', default=[], nargs='+',
-		    help='Only record channels that match channel path string,'
-		    ' e.g. 005/5/2/3/* saves data for all bolometers on crate'
-		    ' 005, slot 5, mezzanine 2, module 3.')
+                    help='Only record channels that match channel path string,'
+                    ' e.g. 005/5/2/3/* saves data for all bolometers on crate'
+                    ' 005, slot 5, mezzanine 2, module 3.')
 parser.add_argument('-b', dest='state', action='store', default=[], nargs='+',
-		    help='Only record bolometers that have a state that'
-		    ' matches the supplied string(s), e.g. overbiased tuned')
+                    help='Only record bolometers that have a state that'
+                    ' matches the supplied string(s), e.g. overbiased tuned')
 args = parser.parse_args()
 
 
@@ -34,26 +34,26 @@ import pydfmux
 print('Initializing hardware map and boards')
 hwm = pydfmux.load_session(open(args.hardware_map, 'r'))['hardware_map']
 if hwm.query(pydfmux.IceCrate).count() > 0:
-        hwm.query(pydfmux.IceCrate).resolve()
+    hwm.query(pydfmux.IceCrate).resolve()
 
 if args.align:
-	print('Aligning board sampling, this will break any existing DAN loops!')
-	hwm.query(pydfmux.IceBoard).set_fir_stage(6)
-	hwm.query(pydfmux.IceBoard).align_sampling()
+    print('Aligning board sampling, this will break any existing DAN loops!')
+    hwm.query(pydfmux.IceBoard).set_fir_stage(6)
+    hwm.query(pydfmux.IceBoard).align_sampling()
 
 print('Beginning data acquisition')
 
 # get board serial numbers only for the channels that we are recording
 if args.pathstring:
-	chan_map_query = hwm.channel_maps_from_pstring(args.pathstring)
+    chan_map_query = hwm.channel_maps_from_pstring(args.pathstring)
 else:
-	chan_map_query = hwm.query(pydfmux.ChannelMapping)
+    chan_map_query = hwm.query(pydfmux.ChannelMapping)
 if args.state:
-	for bolo in hwm.query(pydfmux.Bolometer):
-                if bolo.readout_channel:
-			bolo.state = bolo.retrieve_bolo_state().state
-	hwm.commit()
-	chan_map_query = chan_map_query.join(pydfmux.ChannelMapping, pydfmux.Bolometer).filter(pydfmux.Bolometer.state._in(args.state))
+    for bolo in hwm.query(pydfmux.Bolometer):
+        if bolo.readout_channel:
+            bolo.state = bolo.retrieve_bolo_state().state
+    hwm.commit()
+    chan_map_query = chan_map_query.join(pydfmux.ChannelMapping, pydfmux.Bolometer).filter(pydfmux.Bolometer.state._in(args.state))
 serial_list = np.unique(np.array([cm.iceboard.serial for cm in chan_map_query]))
 
 # Set up DfMux consumer
@@ -64,9 +64,9 @@ builder = dfmux.DfMuxBuilder([int(serial) for serial in serial_list])
 # connections. Using a set rather than a list deduplicates the results.
 local_ips = set()
 for board in hwm.query(pydfmux.core.dfmux.IceBoard):
-	testsock = socket.create_connection(('iceboard' + board.serial + '.local', 80))
-	local_ips.add(testsock.getsockname()[0])
-	testsock.close()
+    testsock = socket.create_connection(('iceboard' + board.serial + '.local', 80))
+    local_ips.add(testsock.getsockname()[0])
+    testsock.close()
 print('Creating listeners for %d boards on interfaces: %s' % (hwm.query(pydfmux.core.dfmux.IceBoard).count(), ', '.join(local_ips)))
 
 # Build mapping dictionary for old (64x) firmware
@@ -81,57 +81,57 @@ pipe.Add(builder)
 pipe.Add(dfmux.PyDfMuxHardwareMapInjector, pydfmux_hwm=hwm, pathstring=args.pathstring, state=args.state)
 
 if args.physnames:
-	from spt3g import calibration
-	# Swap the normal bolometer names for their physical_name equivalents,
-	# which can make laboratory tasks simpler -- we don't care about
-	# long-term data archiving here.
-	pipe.Add(dfmux.PyDfMuxBolometerPropertiesInjector, pydfmux_hwm=hwm)
+    from spt3g import calibration
+    # Swap the normal bolometer names for their physical_name equivalents,
+    # which can make laboratory tasks simpler -- we don't care about
+    # long-term data archiving here.
+    pipe.Add(dfmux.PyDfMuxBolometerPropertiesInjector, pydfmux_hwm=hwm)
 
-	class SwapPhysLogicalNames(object):
-		def __init__(self):
-			self.calframe = None
-			self.wiringframe = None
-			self.sent = False
+    class SwapPhysLogicalNames(object):
+        def __init__(self):
+            self.calframe = None
+            self.wiringframe = None
+            self.sent = False
 
-		def __call__(self, frame):
-			if self.sent:
-				return
-			if frame.type == core.G3FrameType.Wiring:
-				self.wiringframe = frame
-				return []
-			if frame.type == core.G3FrameType.Calibration:
-				self.calframe = frame
-				return []
-			if self.calframe is None or self.wiringframe is None:
-				raise Exception('Data before wiring and cal frames!')
-			w = self.wiringframe['WiringMap']
-			del self.wiringframe['WiringMap']
-			c = self.calframe['NominalBolometerProperties']
+        def __call__(self, frame):
+            if self.sent:
+                return
+            if frame.type == core.G3FrameType.Wiring:
+                self.wiringframe = frame
+                return []
+            if frame.type == core.G3FrameType.Calibration:
+                self.calframe = frame
+                return []
+            if self.calframe is None or self.wiringframe is None:
+                raise Exception('Data before wiring and cal frames!')
+            w = self.wiringframe['WiringMap']
+            del self.wiringframe['WiringMap']
+            c = self.calframe['NominalBolometerProperties']
 
-			new_wiring = dfmux.DfMuxWiringMap()
-			for k, d in w.iteritems():
-				new_wiring[c[k].physical_name] = d
-			self.wiringframe['WiringMap'] = new_wiring
+            new_wiring = dfmux.DfMuxWiringMap()
+            for k, d in w.iteritems():
+                new_wiring[c[k].physical_name] = d
+            self.wiringframe['WiringMap'] = new_wiring
 
-			self.sent = True
-			return [self.wiringframe, frame]
-	pipe.Add(SwapPhysLogicalNames)
+            self.sent = True
+            return [self.wiringframe, frame]
+    pipe.Add(SwapPhysLogicalNames)
 
 if args.system_time:
-	def sub_system_time(frame):
-		if frame.type != core.G3FrameType.Timepoint:
-			return
-		del frame['EventHeader']
-		frame['EventHeader'] = core.G3Time.Now()
-	pipe.Add(sub_system_time)
+    def sub_system_time(frame):
+        if frame.type != core.G3FrameType.Timepoint:
+            return
+        del frame['EventHeader']
+        frame['EventHeader'] = core.G3Time.Now()
+    pipe.Add(sub_system_time)
 
 if args.verbose:
-	pipe.Add(core.Dump)
+    pipe.Add(core.Dump)
 
 
 pipe.Add(dfmux.NetCDFDump, filename=args.output)
 
 for collector in collectors:
-	collector.Start()
+    collector.Start()
 pipe.Run()
 
--- dfmux/onlinescripts/teeledgerman.py
+++ dfmux/onlinescripts/teeledgerman.py
@@ -12,35 +12,35 @@ parser.add_argument('-m', dest='hardware_map', default=None,
                     help='Path to hardware map YAML file. Uses the network version if unspecified.')
 
 parser.add_argument('-v', dest='verbose', action='store_true',
-		    help='Verbose mode (print all frames)')
+                    help='Verbose mode (print all frames)')
 parser.add_argument('-P', dest='physnames', action='store_true',
-		    help='Use physical bolometer names rather than channel names')
+                    help='Use physical bolometer names rather than channel names')
 parser.add_argument('-p', dest='pathstring', action='store', default=[], nargs='+',
-		    help='Only record channels that match channel path string,'
-		    ' e.g. with a local pydfmux hardware map, 005/5/2/3/*'
-		    ' saves data for all bolometers on crate 005, slot 5, '
-		    'mezzanine 2, module 3. When used with a remote hardware '
+                    help='Only record channels that match channel path string,'
+                    ' e.g. with a local pydfmux hardware map, 005/5/2/3/*'
+                    ' saves data for all bolometers on crate 005, slot 5, '
+                    'mezzanine 2, module 3. When used with a remote hardware '
                     'map, applied as a glob to the stored bolometer names.')
 parser.add_argument('-b', dest='state', action='store', default=[], nargs='+',
-		    help='Only record bolometers that have a state that'
-		    ' matches the supplied string(s), e.g. overbiased tuned')
+                    help='Only record bolometers that have a state that'
+                    ' matches the supplied string(s), e.g. overbiased tuned')
 args = parser.parse_args()
 
 
 if args.hardware_map is not None:
-	# Import pydfmux later since it can take a while
-	import pydfmux
+    # Import pydfmux later since it can take a while
+    import pydfmux
 
-	print('Initializing hardware map and boards')
-	hwm = pydfmux.load_session(open(args.hardware_map, 'r'))['hardware_map']
-	if hwm.query(pydfmux.IceCrate).count() > 0:
-		hwm.query(pydfmux.IceCrate).resolve()
+    print('Initializing hardware map and boards')
+    hwm = pydfmux.load_session(open(args.hardware_map, 'r'))['hardware_map']
+    if hwm.query(pydfmux.IceCrate).count() > 0:
+        hwm.query(pydfmux.IceCrate).resolve()
 
-	# Retrieve pydfmux state
-	if args.state:
-		for bolo in hwm.query(pydfmux.Bolometer):
-			if bolo.readout_channel:
-				bolo.state = bolo.retrieve_bolo_state().state
+    # Retrieve pydfmux state
+    if args.state:
+        for bolo in hwm.query(pydfmux.Bolometer):
+            if bolo.readout_channel:
+                bolo.state = bolo.retrieve_bolo_state().state
 
 print('Beginning data acquisition')
 
@@ -49,73 +49,73 @@ pipe = core.G3Pipeline()
 pipe.Add(core.G3Reader, filename='tcp://' + args.server + ':' + str(args.port))
 
 if args.hardware_map is not None:
-	# Insert current hardware map, replacing whatever we got from the server,
-	# if user requests it
-	pipe.Add(lambda fr: fr.type != core.G3FrameType.Wiring)
-	pipe.Add(dfmux.PyDfMuxHardwareMapInjector, pydfmux_hwm=hwm, pathstring=args.pathstring, state=args.state)
+    # Insert current hardware map, replacing whatever we got from the server,
+    # if user requests it
+    pipe.Add(lambda fr: fr.type != core.G3FrameType.Wiring)
+    pipe.Add(dfmux.PyDfMuxHardwareMapInjector, pydfmux_hwm=hwm, pathstring=args.pathstring, state=args.state)
 
-	if args.physnames:
-		# Need bolo properties below
-		pipe.Add(dfmux.PyDfMuxBolometerPropertiesInjector, pydfmux_hwm=hwm)
+    if args.physnames:
+        # Need bolo properties below
+        pipe.Add(dfmux.PyDfMuxBolometerPropertiesInjector, pydfmux_hwm=hwm)
 
 if args.physnames:
-	from spt3g import calibration
-	# Swap the normal bolometer names for their physical_name equivalents,
-	# which can make laboratory tasks simpler -- we don't care about
-	# long-term data archiving here.
-	class SwapPhysLogicalNames(object):
-		def __init__(self):
-			self.calframe = None
-			self.wiringframe = None
-			self.sent = False
+    from spt3g import calibration
+    # Swap the normal bolometer names for their physical_name equivalents,
+    # which can make laboratory tasks simpler -- we don't care about
+    # long-term data archiving here.
+    class SwapPhysLogicalNames(object):
+        def __init__(self):
+            self.calframe = None
+            self.wiringframe = None
+            self.sent = False
 
-		def __call__(self, frame):
-			if self.sent:
-				return
-			if frame.type == core.G3FrameType.Wiring:
-				self.wiringframe = frame
-				return []
-			elif frame.type == core.G3FrameType.Calibration:
-				self.calframe = frame
-				return []
-			elif frame.type != core.G3FrameType.Timepoint:
-				# Just pass this along...
-				return
+        def __call__(self, frame):
+            if self.sent:
+                return
+            if frame.type == core.G3FrameType.Wiring:
+                self.wiringframe = frame
+                return []
+            elif frame.type == core.G3FrameType.Calibration:
+                self.calframe = frame
+                return []
+            elif frame.type != core.G3FrameType.Timepoint:
+                # Just pass this along...
+                return
 
-			if self.calframe is None or self.wiringframe is None:
-				raise Exception('Data before wiring and cal frames!')
-			w = self.wiringframe['WiringMap']
-			del self.wiringframe['WiringMap']
-			c = self.calframe['NominalBolometerProperties']
+            if self.calframe is None or self.wiringframe is None:
+                raise Exception('Data before wiring and cal frames!')
+            w = self.wiringframe['WiringMap']
+            del self.wiringframe['WiringMap']
+            c = self.calframe['NominalBolometerProperties']
 
-			new_wiring = dfmux.DfMuxWiringMap()
-			for k, d in w.iteritems():
-				new_wiring[c[k].physical_name] = d
-			self.wiringframe['WiringMap'] = new_wiring
+            new_wiring = dfmux.DfMuxWiringMap()
+            for k, d in w.iteritems():
+                new_wiring[c[k].physical_name] = d
+            self.wiringframe['WiringMap'] = new_wiring
 
-			self.sent = True
-			return [self.wiringframe, frame]
-	pipe.Add(SwapPhysLogicalNames)
-			
+            self.sent = True
+            return [self.wiringframe, frame]
+    pipe.Add(SwapPhysLogicalNames)
+
 if args.hardware_map is None and len(args.pathstring) > 0:
-	def trim_wiring_map(fr):
-		if fr.type != core.G3FrameType.Wiring:
-			return
+    def trim_wiring_map(fr):
+        if fr.type != core.G3FrameType.Wiring:
+            return
 
-		fr['OriginalWiringMap'] = fr['WiringMap']
-		del fr['WiringMap']
-		keys_to_keep = set()
-		for item in args.pathstring:
-			keys_to_keep |= set(fnmatch.filter(fr['OriginalWiringMap'].keys(), item))
-		new_wiring = dfmux.DfMuxWiringMap()
-		for k in keys_to_keep:
-			new_wiring[k] = fr['OriginalWiringMap'][k]
-		fr['WiringMap'] = new_wiring
-		print('Taking data from %d/%d detectors' % (len(keys_to_keep), len(fr['OriginalWiringMap'])))
-	pipe.Add(trim_wiring_map)
+        fr['OriginalWiringMap'] = fr['WiringMap']
+        del fr['WiringMap']
+        keys_to_keep = set()
+        for item in args.pathstring:
+            keys_to_keep |= set(fnmatch.filter(fr['OriginalWiringMap'].keys(), item))
+        new_wiring = dfmux.DfMuxWiringMap()
+        for k in keys_to_keep:
+            new_wiring[k] = fr['OriginalWiringMap'][k]
+        fr['WiringMap'] = new_wiring
+        print('Taking data from %d/%d detectors' % (len(keys_to_keep), len(fr['OriginalWiringMap'])))
+    pipe.Add(trim_wiring_map)
 
 if args.verbose:
-	pipe.Add(core.Dump)
+    pipe.Add(core.Dump)
 
 pipe.Add(dfmux.NetCDFDump, filename=args.output)
 pipe.Run()
