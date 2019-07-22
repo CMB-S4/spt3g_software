5e9f78d69bf96948a4f133bc23cf4c65b98dfa01
Sasha Rahlin <arahlin@fnal.gov>
2019-07-22 13:02:42 -0500

    make interpolation option for reproj_map and reproj_fullsky_healpix_map

--- coordinateutils/include/coordinateutils/maputils.h
+++ coordinateutils/include/coordinateutils/maputils.h
@@ -7,10 +7,10 @@
 
 void get_ra_dec_map_cpp(G3SkyMapConstPtr m, G3SkyMapPtr ra, G3SkyMapPtr dec);
 
-void reproj_map(G3SkyMapConstPtr in_map, G3SkyMapPtr out_map, int rebin=1);
+void reproj_map(G3SkyMapConstPtr in_map, G3SkyMapPtr out_map, int rebin=1, bool interp=false);
 
 void reproj_fullsky_healpix_map(std::vector<double> in_map,
-    G3SkyMapPtr out_map, bool nest=false, int rebin=1);
+    G3SkyMapPtr out_map, bool nest=false, int rebin=1, bool interp=false);
 
 void maputils_pybindings(void);
 
