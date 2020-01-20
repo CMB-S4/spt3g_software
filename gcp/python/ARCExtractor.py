133a29f5ab911ff064f771d55c288a5ed57a5ca1
Sasha Rahlin <arahlin@fnal.gov>
2020-01-20 17:59:37 +1300

    Extract a minimal amount of data from a GCPSlow frame
    This allows more efficient processing of arcfile data when no calibration or
    re-packaging of data are necessary (e.g. searching for observation or schedule
    start/stop times).

--- gcp/python/ARCExtractor.py
+++ gcp/python/ARCExtractor.py
@@ -182,9 +182,9 @@ def UnpackACUData(f):
     f['ACUStatus'] = a
 
 @core.indexmod
-def UnpackTrackerData(f, rewrite_source_from_feature_bits=True):
+def UnpackTrackerMinimal(f, rewrite_source_from_feature_bits=True):
     '''
-    Extracts tracker status information to frame. If
+    Construct SourceName and ObservationId keys from frame. If
     rewrite_source_from_feature_bits is True (the default), will try to
     rewrite source names if DecryptFeatureBit() has been run and either
     "elnod", "calibrator", or "noise" is present in the feature bit list
@@ -213,6 +213,19 @@ def UnpackTrackerData(f, rewrite_source_from_feature_bits=True):
     # And observation ID, if present
     if 'obs_id' in f['antenna0']['tracker']:
         f['ObservationID'] = f['antenna0']['tracker']['obs_id']
+
+
+@core.indexmod
+def UnpackTrackerData(f, rewrite_source_from_feature_bits=True):
+    '''
+    Extracts tracker status information to frame. If
+    rewrite_source_from_feature_bits is True (the default), will try to
+    rewrite source names if DecryptFeatureBit() has been run and either
+    "elnod", "calibrator", or "noise" is present in the feature bit list
+    to that value.
+    '''
+
+    UnpackTrackerMinimal(f, rewrite_source_from_feature_bits)
 
     t = TrackerStatus()
     # List comprehensions are due to funny business with G3VectorFrameObject
@@ -363,5 +376,16 @@ def ARCExtract(pipe):
     pipe.Add(DecryptFeatureBit)
     pipe.Add(UnpackTrackerData)
     pipe.Add(AddBenchData)
+
+
+@core.pipesegment
+def ARCExtractMinimal(pipe):
+    '''Extract bare minimum GCP registers into native objects.
+
+    Includes only GCPFeatureBits, SourceName and ObservationID keys.
+    Use ARCExtract to calibrate and extract the complete frame.
+    '''
+    pipe.Add(DecryptFeatureBit)
+    pipe.Add(UnpackTrackerMinimal)
 
 # Need tool for tilt meter next
