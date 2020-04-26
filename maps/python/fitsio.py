fix off-by-one error in flat sky projection code

maps/python/fitsio.py
@@ -317,11 +317,8 @@ def get_wcs(skymap, reset=False):
         skymap.res / core.G3Units.deg,
     ]
     w.wcs.cunit = ['deg', 'deg']
+    w.wcs.crpix = [skymap.x_center + 1, skymap.y_center + 1]
 
-    crpix = [
-        skymap.shape[1] - skymap.x_center + 1,
-        skymap.shape[0] - skymap.y_center + 1
-    ]
     crval = [
         skymap.alpha_center / core.G3Units.deg,
         skymap.delta_center / core.G3Units.deg,
@@ -330,7 +327,6 @@ def get_wcs(skymap, reset=False):
         v = skymap.delta_center / core.G3Units.rad
         w.wcs.set_pv([(2, 1, np.cos(v) ** 2)])
         crval[1] = 0.0
-    w.wcs.crpix = crpix
     w.wcs.crval = crval
 
     skymap._wcs = w
@@ -402,8 +398,8 @@ def parse_wcs_header(header):
     delta_center = crval[order[1]] * core.G3Units.deg
 
     crpix = w.wcs.crpix
-    x_center = header['NAXIS{}'.format(order[0] + 1)] - crpix[order[0]] + 1
-    y_center = header['NAXIS{}'.format(order[1] + 1)] - crpix[order[1]] + 1
+    x_center = crpix[order[0]] - 1
+    y_center = crpix[order[1]] - 1
 
     if wcsproj in ['CEA']:
         for i, m, v in w.wcs.get_pv():

maps/tests/fitsio.py
@@ -47,7 +47,7 @@ for dim in [300, 301]:
         w = fm2.wcs
         if verbose:
             print(repr(w.to_header()))
-        pixs = (np.array([[0, 0], [0, 1], [0.5, 0.5], [1, 0], [1, 1]]) * dim).astype(int).astype(float)
+        pixs = np.array([[0, 0], [0, dim - 1], [dim // 2, dim // 2], [dim - 1, 0], [dim - 1, dim - 1]]).astype(float)
         ra, dec = maps.get_ra_dec_map(fm2)
 
         angs = []
@@ -63,7 +63,8 @@ for dim in [300, 301]:
                     pass
                 assert(np.allclose(wcs_ang, g3_ang))
             except AssertionError:
-                print(pix, [ra[idx] / deg, dec[idx] / deg], g3_ang, wcs_ang, np.abs(g3_ang - wcs_ang))
+                print('pix', pix, 'pix2ang', [ra[idx] / deg, dec[idx] / deg],
+                      'g3', g3_ang, 'wcs', wcs_ang, 'diff', np.abs(g3_ang - wcs_ang))
                 error += '\nProj{}: xy_to_angle error'.format(p)
             angs.append(g3_ang * deg)
 
@@ -74,7 +75,8 @@ for dim in [300, 301]:
                 assert(np.allclose(g3_pix, pix)) # round trip
                 assert(np.allclose(wcs_pix, g3_pix))
             except AssertionError:
-                print(ang / deg, g3_pix, wcs_pix, np.abs(g3_pix - wcs_pix))
+                print('ang', ang / deg, 'pix', pix, 'g3', g3_pix, 'wcs', wcs_pix,
+                      'diff', np.abs(g3_pix - wcs_pix))
                 error += '\nProj{}: angle_to_xy error'.format(p)
 
         if verbose: