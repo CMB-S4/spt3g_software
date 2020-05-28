6920a65f5d37184df3cbb9029488589c7b7f6406
Sasha Rahlin <arahlin@fnal.gov>
2020-05-28 16:28:11 -0500

    adding missing numpy bindings for various float and integer vector types

--- core/src/python.cxx
+++ core/src/python.cxx
@@ -223,7 +223,7 @@ BOOST_PP_SEQ_FOR_EACH(UNITS_INTERFACE,~,UNITS)
 struct __XXX_fake_g3units_namespace_XXX {};
 
 // Nonsense boilerplate for POD vector numpy bindings
-#define numpy_vector_infrastructure(T, conv) \
+#define numpy_vector_infrastructure(T, name, conv) \
 template <> \
 boost::shared_ptr<std::vector<T> > \
 container_from_object(boost::python::object v) \
@@ -231,34 +231,41 @@ container_from_object(boost::python::object v) \
 	return numpy_container_from_object<std::vector<T> >(v); \
 } \
 static int \
-vector_getbuffer_##T(PyObject *obj, Py_buffer *view, int flags) \
+vector_getbuffer_##name(PyObject *obj, Py_buffer *view, int flags) \
 { \
 	return pyvector_getbuffer<T>(obj, view, flags, conv); \
 } \
-static PyBufferProcs vec_bufferprocs_##T;
+static PyBufferProcs vec_bufferprocs_##name;
 
 #if PY_MAJOR_VERSION < 3
-#define numpy_vector_of(T, desc) \
+#define numpy_vector_of(T, name, desc) \
 { \
 	boost::python::object cls = register_vector_of<T>(desc); \
 	PyTypeObject *vdclass = (PyTypeObject *)cls.ptr(); \
-	vec_bufferprocs_##T.bf_getbuffer = vector_getbuffer_##T; \
-	vdclass->tp_as_buffer = &vec_bufferprocs_##T; \
+	vec_bufferprocs_##name.bf_getbuffer = vector_getbuffer_##name; \
+	vdclass->tp_as_buffer = &vec_bufferprocs_##name; \
 	vdclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER; \
 }
 #else
-#define numpy_vector_of(T, desc) \
+#define numpy_vector_of(T, name, desc) \
 { \
 	boost::python::object cls = register_vector_of<T>(desc); \
 	PyTypeObject *vdclass = (PyTypeObject *)cls.ptr(); \
-	vec_bufferprocs_##T.bf_getbuffer = vector_getbuffer_##T; \
-	vdclass->tp_as_buffer = &vec_bufferprocs_##T; \
+	vec_bufferprocs_##name.bf_getbuffer = vector_getbuffer_##name; \
+	vdclass->tp_as_buffer = &vec_bufferprocs_##name; \
 }
 #endif
 
-numpy_vector_infrastructure(int32_t, "i")
-numpy_vector_infrastructure(double, "d")
-numpy_vector_infrastructure(float, "f")
+numpy_vector_infrastructure(int64_t, int64_t, "q")
+numpy_vector_infrastructure(uint64_t, uint64_t, "Q")
+numpy_vector_infrastructure(int32_t, int32_t, "i")
+numpy_vector_infrastructure(uint32_t, uint32_t, "I")
+numpy_vector_infrastructure(long, long, "l")
+numpy_vector_infrastructure(unsigned long, u_long, "L")
+numpy_vector_infrastructure(double, double, "d")
+numpy_vector_infrastructure(float, float, "f")
+numpy_vector_infrastructure(std::complex<double>, cxdouble, "Zd");
+numpy_vector_infrastructure(std::complex<float>, cxfloat, "Zf");
 
 BOOST_PYTHON_MODULE(core)
 {
@@ -274,13 +281,16 @@ BOOST_PYTHON_MODULE(core)
 
 	// Some POD types
 	register_vector_of<bool>("Bool");
-	register_vector_of<int64_t>("Int64");
-	register_vector_of<uint64_t>("Uint64");
-	numpy_vector_of(int32_t, "Int");
-	register_vector_of<uint32_t>("UInt");
-	numpy_vector_of(double, "Double");
-	register_vector_of<std::complex<double> >("ComplexDouble");
-	numpy_vector_of(float, "Float");
+	numpy_vector_of(int64_t, int64_t, "Int64");
+	numpy_vector_of(uint64_t, uint64_t, "Uint64");
+	numpy_vector_of(int32_t, int32_t, "Int");
+	numpy_vector_of(uint32_t, uint32_t, "UInt");
+	numpy_vector_of(long, long, "Long");
+	numpy_vector_of(unsigned long, u_long, "ULong");
+	numpy_vector_of(double, double, "Double");
+	numpy_vector_of(std::complex<double>, cxdouble, "ComplexDouble");
+	numpy_vector_of(float, float, "Float");
+	numpy_vector_of(std::complex<float>, cxfloat, "ComplexFloat");
 	register_vector_of<std::string>("String");
 	register_vector_of<unsigned char>("UnsignedChar");
 	register_vector_of<G3Time>("G3Time");
