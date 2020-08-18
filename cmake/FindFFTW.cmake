87f70a2129ac4b6fd36afc4120b6cc3901593f4d
Sasha Rahlin <arahlin@fnal.gov>
2020-08-18 11:50:57 -0500

    fix package name warnings with newer versions of cmake

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -15,11 +15,12 @@ find_library (FFTW_LIBRARIES NAMES fftw3 HINTS ENV FFTW_DIR)
 #handles finding thread implementations
 find_library (FFTW_THREADS_LIBRARY NAMES fftw3_threads HINTS ENV FFTW_DIR)
 
+include (FindPackageHandleStandardArgs)
+set(FPHSA_NAME_MISMATCHED TRUE)
 find_package_handle_standard_args(FFTW_THREADS DEFAULT_MSG FFTW_THREADS_LIBRARY)
-
+mark_as_advanced(FFTW_THREADS_LIBRARY)
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
-include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
 mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDES)
