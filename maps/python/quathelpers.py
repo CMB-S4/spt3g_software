13d71d9569ecaa2c28a58cdcf9ccd973832d15a1
Nathan Whitehorn <nwhitehorn@physics.ucla.edu>
2020-10-12 10:57:06 -0400

    Add a utility module (AddTimingToPointingQuats) to add timing to quats.
    This takes a G3VectorQuat and decorates it with timing from some other
    frame key (by default, the scalar raw az timestream), transforming it
    in-place into a G3TimestreamQuat.

--- maps/python/quathelpers.py
+++ maps/python/quathelpers.py
@@ -1,4 +1,4 @@
-from spt3g.core import G3Units, quat, G3VectorQuat, usefulfunc
+from spt3g.core import G3Units, quat, G3VectorQuat, G3TimestreamQuat, usefulfunc, indexmod
 import numpy
 
 
@@ -32,10 +32,12 @@ def quat_to_ang(q):
 
 
 @usefulfunc
-def ang_to_quat(alpha, delta):
+def ang_to_quat(alpha, delt, start=None, stop=None):
     """
     Convert a set of angles (or vector of them) specified as a (longitude,
-    latitude) pair to a pointing quaternion (or vector of them).
+    latitude) pair to a pointing quaternion (or vector of them). If start
+    and stop are defined, the return value for vectors will be a
+    G3TimestreamQuat with start and stop times set to the provided values.
     """
 
     alpha = numpy.asarray(alpha)
@@ -54,4 +56,31 @@ def ang_to_quat(alpha, delta):
     if len(q) == 1:
         return quat(q[0, 0], q[0, 1], q[0, 2], q[0, 3])
     else:
-        return G3VectorQuat(q)
+        if start is not None:
+            out = G3TimestreamQuat(q)
+            out.start = start
+            out.stop = stop
+        else:
+            return G3VectorQuat(q)
+
+@indexmod
+def AddTimingToPointingQuats(fr, key, timing_ref='RawBoresightAz'):
+    """
+    Transforms a quaternion pointing timestream <key> from a G3VectorQuat
+    to a G3TimestreamQuat by adding timing information extracted from some
+    other timestream-like object (scalar pointing timestreams, detector
+    timestreams etc.) specified by <timing_ref>. Because this operation is
+    backwards-compatible and involves no data loss, the transformation is
+    done in-place -- the previous data are deleted and replaced with the new
+    one.
+    """
+    
+    if key not in fr:
+        return
+
+    x = fr.pop(key)
+    x = G3TimestreamQuat(x)
+    x.start = fr[timing_ref].start
+    x.stop = fr[timing_ref].stop
+    fr[key] = x
+
