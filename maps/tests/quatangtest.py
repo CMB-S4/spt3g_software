81e1410080f81aadffbc44c9ccf707a22274d2d5
Sasha Rahlin <arahlin@fnal.gov>
2022-01-14 22:00:26 -0600

    relax equality test for quaternions

--- maps/tests/quatangtest.py
+++ maps/tests/quatangtest.py
@@ -6,15 +6,17 @@ a = core.quat(2,3,4,5)
 a2 = core.quat(2,1,8,5)
 b = core.G3VectorQuat([a, a**2, 2*a, a2])
 
-assert(np.isclose(maps.quat_to_ang(a), maps.c_quat_to_ang_(a)).all())
-assert(np.isclose(maps.quat_to_ang(b), np.asarray([maps.c_quat_to_ang_(x) for x in b]).transpose()).all())
+assert(np.allclose(maps.quat_to_ang(a), maps.c_quat_to_ang_(a)))
+assert(np.allclose(maps.quat_to_ang(b), np.asarray([maps.c_quat_to_ang_(x) for x in b]).transpose()))
 
 angle = (.3, 0.4)
 angles = ((.3, 0.8), (0.4, 0.2))
 
-assert(maps.ang_to_quat(angle[0], angle[1]) == maps.c_ang_to_quat_(angle[0], angle[1]))
+q = maps.ang_to_quat(angle[0], angle[1])
+p = maps.c_ang_to_quat_(angle[0], angle[1])
+assert(np.allclose(core.G3VectorQuat([q]), core.G3VectorQuat([p])))
 alpha, delta = list(zip(angles[0], angles[1]))
-assert((np.asarray(maps.ang_to_quat(alpha, delta)) == np.asarray(core.G3VectorQuat([maps.c_ang_to_quat_(a[0], a[1]) for a in angles]))).all())
+assert(np.allclose(maps.ang_to_quat(alpha, delta), core.G3VectorQuat([maps.c_ang_to_quat_(a[0], a[1]) for a in angles])))
 
-assert(np.isclose(maps.quat_to_ang(maps.c_ang_to_quat_(angle[0], angle[1])), angle).all())
+assert(np.allclose(maps.quat_to_ang(maps.c_ang_to_quat_(angle[0], angle[1])), angle))
 
