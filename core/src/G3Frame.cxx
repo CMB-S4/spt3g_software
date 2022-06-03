be76b17621a3a26f033e5816528182106219ab6d
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-06-03 11:58:46 -0500

    formatting

--- core/src/G3Frame.cxx
+++ core/src/G3Frame.cxx
@@ -276,30 +276,32 @@ template<typename T>
 void G3Frame::saveJSON(T & os) const
 {
 #ifdef SPT3G_ENABLE_JSON_OUTPUT
-  using cereal::make_nvp;
-  uint32_t version(1), size(map_.size());
+	using cereal::make_nvp;
+	uint32_t version(1), size(map_.size());
 
-  cereal::JSONOutputArchive ar(os);
-  ar << make_nvp("version", version);
+	cereal::JSONOutputArchive ar(os);
+	ar << make_nvp("version", version);
 	ar << make_nvp("size", size);
 	std::string typestr(1,(char) type);
 	ar << make_nvp("type", typestr);
-	for (auto i = map_.begin(); i != map_.end(); i++) {
-    //make sure it's deserialized so we don't just write a blob
-    blob_decode(i->second);
+	for (auto i = map_.begin(); i != map_.end(); i++) 
+	{
+		//make sure it's deserialized so we don't just write a blob
+		blob_decode(i->second);
 		ar << make_nvp("name", i->first);
 		ar << make_nvp("val", i->second.frameobject);
 	}
 #else
-  os << " {error: \"spt3g-software compiled without JSON support\"}";
+  os << " {error: \"spt3g-software compiled without JSON support\"}" << std::endl;
 #endif
 }
 
-std::string G3Frame::asJSON() const
+std::string
+G3Frame::asJSON() const
 {
-  std::stringstream str;
-  saveJSON(str);
-  return str.str();
+	std::stringstream str;
+	saveJSON(str);
+	return str.str();
 }
 
 
