f6e792ae251532815817c6dc5ee9e78cc92a1391
Alexandra Rahlin <arahlin@users.noreply.github.com>
2022-09-16 14:01:02 -0500

    More complete set of python bindings for G3Timestream and friends (#84)
    * Implement binary and unary ufuncs for G3Timestream, G3VectorComplexDouble, ComplexDoubleVector, G3VectorDouble, DoubleVector, G3VectorInt, IntVector, G3VectorBool, BoolVector to enable arithmetic operations involving these types or scalars or numpy arrays on either side of each operator.
    * Implement bindings for G3TimestreamMap that mimic the behavior of SO's G3SuperTimestream objects (.times, .data, .names properties).  Note that this also changes the existing `G3TimestreamMap.times()` method into a property attribute.
    
    Fixes #83 (as much as possible)
    
    Co-authored-by: Nicholas Huang <nhuang@alumni.princeton.edu>

---
+++ core/tests/timestream_ops.py
@@ -0,0 +1,155 @@
+import numpy as np
+from spt3g.core import (
+    G3Timestream,
+    G3VectorDouble,
+    DoubleVector,
+    G3VectorInt,
+    IntVector,
+    G3VectorComplexDouble,
+    ComplexDoubleVector,
+    G3VectorBool,
+    BoolVector,
+)
+
+s = 5.0  # python float
+si = 5  # python integer
+sc = s + 1j * s
+v = np.abs(np.random.randn(10)) + 1.0  # numpy array
+vi = np.random.randint(1, 10, 10)
+vc = v + 1j * v  # numpy complex array
+f = v[0]  # numpy float
+fi = vi[0]  # numpy integer
+fc = vc[0]  # numpy complex float
+sb = True
+vb = v.astype(bool)
+fb = vb[0]
+
+all_arr = [
+    s,
+    f,
+    v,
+    si,
+    fi,
+    vi,
+    sc,
+    fc,
+    vc,
+    sb,
+    fb,
+    vb,
+    G3Timestream(v),
+    G3Timestream(vi),
+    G3VectorDouble(v),
+    DoubleVector(v),
+    G3VectorInt(vi),
+    IntVector(vi),
+    G3VectorComplexDouble(vc),
+    ComplexDoubleVector(vc),
+    G3VectorBool(vb),
+    BoolVector(G3VectorBool(vb)),
+]
+
+def check(a1, a2, cls=None):
+    assert np.array_equal(a1, a2)
+    assert np.asarray(a1).dtype.kind == np.asarray(a2).dtype.kind
+    if cls is not None:
+        assert isinstance(a1, cls)
+
+def copy(v):
+    if isinstance(v, np.ndarray):
+        return v.copy()
+    return v.__class__(v)
+
+for a in all_arr:
+    for b in all_arr:
+        n = "/".join([a.__class__.__name__, b.__class__.__name__])
+        if not ("Vector" in n or "Timestream" in n):
+            continue
+
+        print(type(a), type(b), type(a + b))
+        v1 = a if np.isscalar(a) else np.asarray(a)
+        v2 = b if np.isscalar(b) else np.asarray(b)
+        k1 = np.asarray(a).dtype.kind
+        k2 = np.asarray(b).dtype.kind
+
+        # binary operators
+        if not (k1 == 'b' and k2 == 'b'):
+            check(a + b, v1 + v2)
+            check(a - b, v1 - v2)
+            check(a * b, v1 * v2)
+            check(a / b, v1 / v2)
+            if k1 != 'c' and k2 != 'c':
+                check(a // b, v1 // v2)
+                check(a % b, v1 % v2)
+            check(a ** b, v1 ** v2)
+
+        if k1 in 'iub' and k2 in 'iub':
+            check(a | b, v1 | v2)
+            check(a & b, v1 & v2)
+            check(a ^ b, v1 ^ v2)
+            check(a << 1, v1 << 1)
+            check(a >> 1, v1 >> 1)
+
+        cls = None if np.isscalar(a) or isinstance(a, np.ndarray) else G3VectorBool if 'G3' in n else BoolVector
+        check(a < b, v1 < v2, cls)
+        check(a <= b, v1 <= v2, cls)
+        check(a == b, v1 == v2, cls)
+        check(a != b, v1 != v2, cls)
+        check(a >= b, v1 >= v2, cls)
+        check(a > b, v1 > v2, cls)
+
+        if np.isscalar(a) or k1 == 'b':
+            continue
+
+        korder = list('buifc')
+        if korder.index(np.asarray(a).dtype.kind) < korder.index(np.asarray(b).dtype.kind):
+            continue
+
+        # in-place binary operators
+        c = copy(a)
+        c += b
+        check(c, a + b, a.__class__)
+
+        c = copy(a)
+        c -= b
+        check(c, a - b, a.__class__)
+
+        c = copy(a)
+        c *= b
+        check(c, a * b, a.__class__)
+
+        if k1 in 'iub':
+            c = copy(a)
+            c |= b
+            check(c, a | b, a.__class__)
+
+            c = copy(a)
+            c &= b
+            check(c, a & b, a.__class__)
+
+            c = copy(a)
+            c ^= b
+            check(c, a ^ b, a.__class__)
+
+            continue
+
+        c = copy(a)
+        c /= b
+        check(c, a / b, a.__class__)
+
+    if np.isscalar(a) or isinstance(a, np.ndarray):
+        continue
+
+    for attr in [
+        "sum",
+        "mean",
+        "any",
+        "all",
+        "min",
+        "max",
+        "argmin",
+        "argmax",
+        "var",
+        "std",
+    ]:
+        assert getattr(a, attr)() == getattr(np, attr)(v1)
