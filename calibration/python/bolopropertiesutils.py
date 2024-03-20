a36f5b18b4236cd86ad8bd6d53aeb8ca7204191f
Sasha Rahlin <arahlin@fnal.gov>
2024-03-19 23:35:50 -0500

    add band string formatting methods to BolometerProperties class
    add a band_string property attribute to BolometerProperties objects

--- calibration/python/bolopropertiesutils.py
+++ calibration/python/bolopropertiesutils.py
@@ -1,9 +1,83 @@
 from spt3g.calibration import BolometerProperties
 from spt3g import core
 import numpy as np
+import re
 
 __all__ = ['SplitByProperty', 'SplitByBand', 'SplitTimestreamsByBand',
            'SplitByWafer', 'SplitByPixelType']
+
+
+# framework for handling frequency band formatting
+
+BolometerProperties._band_precision = 0
+BolometerProperties._band_units = "GHz"
+
+def get_band_precision(cls):
+    """Bolometer frequency band precision for string formatting"""
+    return cls._band_precision
+def set_band_precision(cls, precision):
+    cls._band_precision = int(precision)
+BolometerProperties.band_precision = property(get_band_precision, set_band_precision)
+
+def get_band_units(cls, units):
+    """Bolometer units name for string formatting"""
+    return cls._band_units
+def set_band_units(cls, units):
+    assert hasattr(core.G3Units, units), "Invalid units {}".format(units)
+    cls._band_units = units
+BolometerProperties.band_units = property(get_band_units, set_band_units)
+
+def band_to_string(cls, band):
+    """Convert a band number in G3Units to its string representation, using the
+    appropriate precision and units name."""
+    precision = cls.band_precision
+    units = cls.band_units
+    band = np.round(band / getattr(core.G3Units, units), precision)
+    return "%%.%df%s" % (precision, units) % band
+BolometerProperties.band_to_string = classmethod(band_to_string)
+
+def band_string(self):
+    """Band string representation"""
+    if not np.isfinite(self.band) or self.band < 0:
+        return ""
+    return self.band_to_string(self.band)
+BolometerProperties.band_string = property(band_string)
+
+def band_to_num(cls, band):
+    """Convert a band string to a number in G3Units, or raise a ValueError if no
+    match is found."""
+    m = cls.band_regex.match(band)
+    if not m:
+        raise ValueError("Invalid band {}".format(band))
+    return float(m.group(1)) * getattr(core.G3Units, units)
+BolometerProperties.band_to_num = classmethod(band_to_num)
+
+def band_regex(cls):
+    """Compiled regular expression for identifying a band string, whose first
+    match group returns the numerical portion of the string."""
+    precision = cls.band_precision
+    units = cls.band_units
+    prx = "\.[0-9]{{}}".format(precision) if precision else ""
+    return re.compile("([0-9]+{}){}".format(prx, units))
+BolometerProperties.band_regex = property(band_regex)
+
+def band_string_from_string(cls, s):
+    """Return the band substring from the input string, or None if not found."""
+    m = cls.band_regex.search(s)
+    if not m:
+        return None
+    return m.group(0)
+BolometerProperties.band_string_from_string = classmethod(band_string_from_string)
+
+def band_from_string(cls, s):
+    """Return the band in G3Units extracted from the input string, or None if
+    not found."""
+    m = cls.band_regex.search(s)
+    if not m:
+        return None
+    return float(m.group(1)) * getattr(core.G3Units, cls.band_units)
+BolometerProperties.band_from_string = classmethod(band_from_string)
+
 
 @core.indexmod
 class SplitByProperty(object):
@@ -113,8 +187,7 @@ class SplitByBand(SplitByProperty):
     G3TimestreamMap, G3MapInt, etc.
     '''
     def __init__(self, input='CalTimestreams', output_root=None,
-                 bands=None, bpm='BolometerProperties', drop_empty=False,
-                 precision=0):
+                 bands=None, bpm='BolometerProperties', drop_empty=False):
         '''
         Split the input map given by input into several output
         maps named output_root + band + GHz (e.g. CalTimestreams150GHz with
@@ -123,22 +196,18 @@ class SplitByBand(SplitByProperty):
         creates maps for every band that exists in the input. Setting bpm
         to a non-default value causes this to get its band mapping from an
         alternative data source.
-
-        If precision > 0, then store the band string with this many decimal
-        places of precision.  Otherwise, assume an integer.
         '''
         super(SplitByBand, self).__init__(
             input=input, output_root=output_root, property_list=bands,
-            bpm=bpm, property='band', drop_empty=drop_empty)
-        self.precision = precision
+            bpm=bpm, property='band_string', drop_empty=drop_empty)
 
-    def converter(self, band):
-        if isinstance(band, str):
-            return band
-        if not np.isfinite(band) or band < 0:
+    @staticmethod
+    def converter(band_string):
+        if band_string is None:
             return None
-        band = np.round(band / core.G3Units.GHz, self.precision)
-        return '%%.%dfGHz' % self.precision % band
+        if not band_string:
+            return None
+        return str(band_string)
 
 
 @core.indexmod
