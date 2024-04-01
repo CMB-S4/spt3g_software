c1a51057b6468aeae095b8e81e25af267613567c
Sasha Rahlin <arahlin@fnal.gov>
2024-04-01 16:13:14 -0500

    test negative precision

--- calibration/tests/band_format.py
+++ calibration/tests/band_format.py
@@ -2,7 +2,7 @@ from spt3g import core
 from spt3g.calibration import BolometerProperties, set_band_format, band_to_value, get_band_units
 
 bp = BolometerProperties()
-bp.band = 95.47 * core.G3Units.GHz
+bp.band = 94.67 * core.G3Units.GHz
 
 assert bp.band_string == "95GHz"
 assert bp.band_vstring == "95"
@@ -11,14 +11,20 @@ assert bp.band_vstring + get_band_units() == bp.band_string
 
 set_band_format(0, "MHz")
 
-assert bp.band_string == "95470MHz"
-assert bp.band_vstring == "95470"
+assert bp.band_string == "94670MHz"
+assert bp.band_vstring == "94670"
 assert band_to_value(bp.band_string) == bp.band
 assert bp.band_vstring + get_band_units() == bp.band_string
 
 set_band_format(2, "GHz")
 
-assert bp.band_string == "95.47GHz"
-assert bp.band_vstring == "95.47"
+assert bp.band_string == "94.67GHz"
+assert bp.band_vstring == "94.67"
 assert band_to_value(bp.band_string) == bp.band
+assert bp.band_vstring + get_band_units() == bp.band_string
+
+set_band_format(-1, "GHz")
+assert bp.band_string == "90GHz"
+assert bp.band_vstring == "90"
+assert band_to_value(bp.band_string) == 90 * core.G3Units.GHz
 assert bp.band_vstring + get_band_units() == bp.band_string
