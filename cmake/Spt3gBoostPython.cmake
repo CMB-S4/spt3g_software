abde7da860e2ce04e751676008d3a4874f0ae5a7
Sasha Rahlin <arahlin@uchicago.edu>
2024-09-10 13:25:47 -0500

    libraries variable already contains the right targets

--- cmake/Spt3gBoostPython.cmake
+++ cmake/Spt3gBoostPython.cmake
@@ -71,3 +71,4 @@ if(NOT DEFINED Boost_PYTHON_TYPE)
 endif()
 
 find_package(Boost COMPONENTS system iostreams filesystem ${Boost_PYTHON_TYPE} REQUIRED)
+message(STATUS "Found Boost: ${Boost_LIBRARIES}")
