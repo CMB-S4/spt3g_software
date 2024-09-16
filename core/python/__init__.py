e628176dacb31d2a79f813635998ee45fc71f43c
Sasha Rahlin <arahlin@uchicago.edu>
2024-09-16 13:07:56 -0500

    Relative imports in core init

--- core/python/__init__.py
+++ core/python/__init__.py
@@ -1,7 +1,7 @@
-from spt3g.core.load_pybindings import load_pybindings
+from .load_pybindings import load_pybindings
 load_pybindings(__name__, __path__)
 
-from spt3g.core.g3logging import log_trace, log_debug, log_info, log_notice, log_warn, log_error, log_fatal, set_log_level
+from .g3logging import log_trace, log_debug, log_info, log_notice, log_warn, log_error, log_fatal, set_log_level
 
 import atexit
 def fix_logging_crash():
@@ -11,22 +11,22 @@ def fix_logging_crash():
 atexit.register(fix_logging_crash)
 del fix_logging_crash
 
-from spt3g.core.fileio import *
-from spt3g.core.modconstruct import pipesegment, indexmod, pipesegment_nodoc
-from spt3g.core.funcconstruct import usefulfunc
+from .fileio import *
+from .modconstruct import pipesegment, indexmod, pipesegment_nodoc
+from .funcconstruct import usefulfunc
 try:
-	from spt3g.core.parse_pipeline_graph import plot_frame_processing_info
+	from .parse_pipeline_graph import plot_frame_processing_info
 except ImportError:
 	pass
 try:
-	from spt3g.core.multiprocess import Subproc
+	from .multiprocess import Subproc
 except ImportError:
 	pass
-from spt3g.core.util import *
+from .util import *
 
-from spt3g.core.docparser import *
-from spt3g.core.dataextensions import *
-from spt3g.core.frameextensions import *
-from spt3g.core.timestreamextensions import *
+from .docparser import *
+from .dataextensions import *
+from .frameextensions import *
+from .timestreamextensions import *
 
-from spt3g.core.g3decorators import cache_frame_data, scan_func_cache_data
+from .g3decorators import cache_frame_data, scan_func_cache_data
