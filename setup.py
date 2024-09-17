5839ff1ad3e0347cd62941a3837744ab2fe5b800
Sasha Rahlin <arahlin@uchicago.edu>
2024-09-16 22:52:15 -0500

    setup defaults

--- setup.py
+++ setup.py
@@ -29,6 +29,13 @@ class CMakeBuild(build_ext):
         if not any(["Python_ROOT_DIR" in a for a in cmake_args]):
             pyroot = sysconfig.get_config_var("prefix")
             cmake_args += [f"-DPython_ROOT_DIR={pyroot}"]
+        if not any(["Python_EXECUTABLE" in a for a in cmake_args]):
+            cmake_args += [f"-DPython_EXECUTABLE={sys.executable}"]
+        if not any(["Python_INCLUDE_DIR" in a for a in cmake_args]):
+            pyinc = sysconfig.get_config_var("INCLUDEDIR")
+            cmake_args += [f"-DPython_INCLUDE_DIR={pyinc}"]
+        if not any(["Boost_PYTHON_TYPE" in a for a in cmake_args]):
+            cmake_args += [f"-DBoost_PYTHON_TYPE=python"]
 
         build_temp = Path("wheel/build" if self.editable_mode else self.build_temp)
         if not build_temp.exists():
