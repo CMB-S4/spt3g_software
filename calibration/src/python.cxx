aebc7427c814694635330c0b77213ab21c614f24
Sasha Rahlin <arahlin@uchicago.edu>
2024-09-18 09:14:00 -0500

    keep weird imports?

--- calibration/src/python.cxx
+++ calibration/src/python.cxx
@@ -4,6 +4,9 @@ namespace bp = boost::python;
 
 SPT3G_PYTHON_MODULE(calibration)
 {
+	// Python bindings dependencies
+	bp::import("spt3g.core");
+
 	// Disable noise in doc strings
 	bp::docstring_options docopts(true, true, false);
 
--- gcp/src/python.cxx
+++ gcp/src/python.cxx
@@ -9,6 +9,7 @@ namespace bp = boost::python;
 
 SPT3G_PYTHON_MODULE(gcp)
 {
+	bp::import("spt3g.core");
 	bp::docstring_options docopts(true, true, false);
 
 	// Supported Experiments
