e2569218105f9a1a2f6ed1239063ce42c54eba47
Sasha Rahlin <arahlin@fnal.gov>
2024-09-18 02:13:36 -0500

    remove numpy/scipy cmake checks

--- cmake/Spt3gBoostPython.cmake
+++ cmake/Spt3gBoostPython.cmake
@@ -12,39 +12,6 @@ else()
 	set(Python_EXECUTABLE ${PYTHON_EXECUTABLE})
 endif()
 
-# look for numpy
-execute_process(COMMAND ${Python_EXECUTABLE} -c "import numpy"
-	RESULT_VARIABLE NUMPY_FOUND ERROR_QUIET)
-if(NUMPY_FOUND EQUAL 0)
-	set(Python_NumPy_FOUND TRUE CACHE BOOL "Numpy found successfully" FORCE)
-else(NUMPY_FOUND EQUAL 0)
-	set(Python_NumPy_FOUND FALSE CACHE BOOL "Numpy found successfully" FORCE)
-endif(NUMPY_FOUND EQUAL 0)
-
-if(Python_NumPy_FOUND)
-	execute_process(COMMAND ${Python_EXECUTABLE} -c
-		"import numpy; print(numpy.get_include())"
-		OUTPUT_VARIABLE _NUMPY_INCLUDE_DIR
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-	find_path(NUMPY_INCLUDE_DIR NAMES numpy/ndarrayobject.h HINTS ${_NUMPY_INCLUDE_DIR})
-	set(Python_NumPy_INCLUDE_DIRS ${NUMPY_INCLUDE_DIR} CACHE STRING "Numpy inc directory")
-	message(STATUS "Found NumPy: ${Python_NumPy_INCLUDE_DIRS}")
-else(Python_NumPy_FOUND)
-	message(STATUS "Found NumPy: NOT FOUND")
-endif(Python_NumPy_FOUND)
-
-## look for scipy
-execute_process(COMMAND ${Python_EXECUTABLE} -c "import scipy"
-	RESULT_VARIABLE SCIPY_FOUND OUTPUT_QUIET ERROR_QUIET)
-if(SCIPY_FOUND EQUAL 0)
-	set(Python_SciPy_FOUND TRUE CACHE BOOL "SciPy Found")
-	message(STATUS "Found SciPy: FOUND")
-else()
-	set(Python_SciPy_FOUND FALSE CACHE BOOL "SciPy Found")
-	message(STATUS "Found SciPy: NOT FOUND")
-endif()
-
 # suppress configuration warnings in newer cmake / boost versions
 if(NOT DEFINED CMAKE_FIND_PACKAGE_PREFER_CONFIG)
 	set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
