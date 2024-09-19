eeab5cb4e9de330d17fa2e99e443f0d9fd986b8e
Sasha Rahlin <arahlin@uchicago.edu>
2024-09-19 00:38:18 -0500

    clean up python.cxx: vectors

--- core/src/G3Vector.cxx
+++ core/src/G3Vector.cxx
@@ -5,6 +5,122 @@
 #include <complex>
 #include "int_storage.h"
 
+// Nonsense boilerplate for POD vector numpy bindings
+#define numpy_vector_struct(T, name) \
+struct numpy_vector_from_python_##name { \
+	numpy_vector_from_python_##name() { \
+		boost::python::converter::registry::push_back( \
+		    &convertible, &construct, \
+		    boost::python::type_id<std::vector<T> >()); \
+	} \
+	static void *convertible(PyObject* obj_ptr) { \
+		Py_buffer view; \
+		if (PyObject_GetBuffer(obj_ptr, &view, \
+		    PyBUF_FORMAT | PyBUF_ANY_CONTIGUOUS) == -1) { \
+			PyErr_Clear(); \
+			return NULL; \
+		} \
+		if (view.ndim == 0) { \
+			PyBuffer_Release(&view); \
+			return NULL; \
+		} \
+		PyBuffer_Release(&view); \
+		return obj_ptr; \
+	} \
+	static void construct(PyObject* obj_ptr, \
+	    boost::python::converter::rvalue_from_python_stage1_data* data) { \
+		void* storage = ( \
+		    (boost::python::converter::rvalue_from_python_storage<std::vector<T> >*)data)->storage.bytes; \
+		new (storage) std::vector<T>; \
+		boost::shared_ptr<std::vector<T> > swap_storage = numpy_container_from_object<std::vector<T> >(boost::python::object(boost::python::handle<>(boost::python::borrowed(obj_ptr)))); \
+		((std::vector<T> *)(storage))->swap(*swap_storage); \
+		data->convertible = storage; \
+	} \
+};
+
+#define numpy_vector_infrastructure(T, name, conv) \
+template <> \
+boost::shared_ptr<std::vector<T> > \
+container_from_object(boost::python::object v) \
+{ \
+	return numpy_container_from_object<std::vector<T> >(v); \
+} \
+static int \
+vector_getbuffer_##name(PyObject *obj, Py_buffer *view, int flags) \
+{ \
+	return pyvector_getbuffer<T>(obj, view, flags, conv); \
+} \
+static PyBufferProcs vec_bufferprocs_##name; \
+numpy_vector_struct(T, name)
+
+#if PY_MAJOR_VERSION < 3
+#define numpy_vector_of(T, name, desc) \
+{ \
+	numpy_vector_from_python_##name(); \
+	boost::python::object cls = register_vector_of<T>(desc); \
+	PyTypeObject *vdclass = (PyTypeObject *)cls.ptr(); \
+	vec_bufferprocs_##name.bf_getbuffer = vector_getbuffer_##name; \
+	vdclass->tp_as_buffer = &vec_bufferprocs_##name; \
+	vdclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER; \
+}
+#else
+#define numpy_vector_of(T, name, desc) \
+{ \
+	numpy_vector_from_python_##name(); \
+	boost::python::object cls = register_vector_of<T>(desc); \
+	PyTypeObject *vdclass = (PyTypeObject *)cls.ptr(); \
+	vec_bufferprocs_##name.bf_getbuffer = vector_getbuffer_##name; \
+	vdclass->tp_as_buffer = &vec_bufferprocs_##name; \
+}
+#endif
+
+numpy_vector_infrastructure(int64_t, int64_t, "q")
+numpy_vector_infrastructure(uint64_t, uint64_t, "Q")
+numpy_vector_infrastructure(int32_t, int32_t, "i")
+numpy_vector_infrastructure(uint32_t, uint32_t, "I")
+numpy_vector_infrastructure(double, double, "d")
+numpy_vector_infrastructure(float, float, "f")
+
+// Apple, for their own insane reasons, defines uint64_t as
+// "unsigned long long" even on LP64 systems where longs are
+// 64-bit. Because "long long" (not a standard C type!) is not
+// actually the same type as "long", even when both are 64-bit
+// integers, the uint64_t definition above does not do the right
+// thing for size_t on 64-bit Apple systems.
+//
+// Thanks, Apple. "Think Different!"
+#if defined(__APPLE__) && defined(__LP64__)
+numpy_vector_struct(size_t, size_t)
+numpy_vector_struct(ssize_t, ssize_t)
+struct apple_size
+{
+	static PyObject* convert(const std::vector<size_t> &arg) {
+		return boost::python::to_python_value<std::vector<uint64_t> >()(*(std::vector<uint64_t> *)(uintptr_t)(&arg));
+	}
+};
+
+struct apple_ssize
+{
+	static PyObject* convert(const std::vector<ssize_t> &arg) {
+		return boost::python::to_python_value<std::vector<int64_t> >()(*(std::vector<int64_t> *)(intptr_t)(&arg));
+	}
+};
+#endif
+
+template <> boost::shared_ptr<std::vector<std::complex<float> > >
+numpy_container_from_object(boost::python::object v)
+{
+	return complex_numpy_container_from_object<std::vector<std::complex<float> > >(v);
+}
+template <> boost::shared_ptr<std::vector<std::complex<double> > >
+numpy_container_from_object(boost::python::object v)
+{
+	return complex_numpy_container_from_object<std::vector<std::complex<double> > >(v);
+}
+
+numpy_vector_infrastructure(std::complex<double>, cxdouble, "Zd");
+numpy_vector_infrastructure(std::complex<float>, cxfloat, "Zf");
+
 G3_SPLIT_SERIALIZABLE_CODE(G3VectorInt);
 G3_SERIALIZABLE_CODE(G3VectorBool);
 G3_SERIALIZABLE_CODE(G3VectorDouble);
@@ -168,6 +284,8 @@ static PyBufferProcs vecint_bufferprocs;
 static PyBufferProcs vectime_bufferprocs;
 
 PYBINDINGS("core") {
+	numpy_vector_of(float, float, "Float");
+	numpy_vector_of(double, double, "Double");
 	boost::python::object vecdouble = register_g3vector<double>(
 	    "G3VectorDouble", "Array of floats. Treat as a serializable "
 	    "version of numpy.array(dtype=float64). Can be efficiently cast "
@@ -179,6 +297,9 @@ PYBINDINGS("core") {
 #if PY_MAJOR_VERSION < 3
 	vdclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER;
 #endif
+
+	numpy_vector_of(std::complex<float>, cxfloat, "ComplexFloat");
+	numpy_vector_of(std::complex<double>, cxdouble, "ComplexDouble");
 	boost::python::object veccomplexdouble =
 	   register_g3vector<std::complex<double> >(
 	    "G3VectorComplexDouble", "Array of complex floats. Treat as a serializable "
@@ -193,6 +314,10 @@ PYBINDINGS("core") {
 	vcclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER;
 #endif
 
+	numpy_vector_of(int64_t, int64_t, "Int64");
+	numpy_vector_of(uint64_t, uint64_t, "UInt64");
+	numpy_vector_of(int32_t, int32_t, "Int");
+	numpy_vector_of(uint32_t, uint32_t, "UInt");
 	boost::python::object vecint = register_g3vector<int64_t>("G3VectorInt",
 	    "Array of integers. Treat as a serializable version of "
 	    "numpy.array(dtype=int64). Can be efficiently cast to and from "
@@ -205,16 +330,31 @@ PYBINDINGS("core") {
 	viclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER;
 #endif
 
+#if defined(__APPLE__) && defined(__LP64__)
+	numpy_vector_from_python_size_t();
+	numpy_vector_from_python_ssize_t();
+	bp::to_python_converter<std::vector<size_t>, apple_size, false>();
+	bp::to_python_converter<std::vector<ssize_t>, apple_ssize, false>();
+#endif
+
+	register_vector_of<bool>("Bool");
 	register_g3vector<bool>("G3VectorBool", "List of booleans.");
+
+	register_vector_of<std::string>("String");
 	register_g3vector<std::string>("G3VectorString", "List of strings.");
 	register_vector_of<G3VectorString>("VectorG3VectorString");
 	register_g3vector<G3VectorString>("G3VectorVectorString", "List of "
 	    "lists of strings.");
+
 	register_g3vector<G3FrameObjectPtr>("G3VectorFrameObject", "List of "
 	    "generic frame objects. Can lead to paradoxes; avoid use of this "
 	    "class unless you are sure you need it.");
+
+	register_vector_of<unsigned char>("UnsignedChar");
 	register_g3vector<uint8_t>("G3VectorUnsignedChar", "List of 8-bit "
 	    "integers");
+
+	register_vector_of<G3Time>("G3Time");
 	boost::python::object vectime =
 	    register_g3vector<G3Time>("G3VectorTime", "List of times.");
 	// Add buffer protocol interface
--- core/src/python.cxx
+++ core/src/python.cxx
@@ -1,10 +1,8 @@
 #include <pybindings.h>
-#include <serialization.h>
 
 #include <boost/python.hpp>
 #include <boost/preprocessor.hpp>
 
-#include <G3TimeStamp.h>
 #include <G3Constants.h>
 
 namespace bp = boost::python;
@@ -134,122 +132,6 @@ BOOST_PP_SEQ_FOR_EACH(CONSTANTS_INTERFACE,~,CONSTANTS)
   .add_static_property(BOOST_PP_STRINGIZE(T), &BOOST_PP_CAT(g3constants_return_, T))
 struct __XXX_fake_g3constants_namespace_XXX {};
 
-// Nonsense boilerplate for POD vector numpy bindings
-#define numpy_vector_struct(T, name) \
-struct numpy_vector_from_python_##name { \
-	numpy_vector_from_python_##name() { \
-		boost::python::converter::registry::push_back( \
-		    &convertible, &construct, \
-		    boost::python::type_id<std::vector<T> >()); \
-	} \
-	static void *convertible(PyObject* obj_ptr) { \
-		Py_buffer view; \
-		if (PyObject_GetBuffer(obj_ptr, &view, \
-		    PyBUF_FORMAT | PyBUF_ANY_CONTIGUOUS) == -1) { \
-			PyErr_Clear(); \
-			return NULL; \
-		} \
-		if (view.ndim == 0) { \
-			PyBuffer_Release(&view); \
-			return NULL; \
-		} \
-		PyBuffer_Release(&view); \
-		return obj_ptr; \
-	} \
-	static void construct(PyObject* obj_ptr, \
-	    boost::python::converter::rvalue_from_python_stage1_data* data) { \
-		void* storage = ( \
-		    (boost::python::converter::rvalue_from_python_storage<std::vector<T> >*)data)->storage.bytes; \
-		new (storage) std::vector<T>; \
-		boost::shared_ptr<std::vector<T> > swap_storage = numpy_container_from_object<std::vector<T> >(boost::python::object(boost::python::handle<>(boost::python::borrowed(obj_ptr)))); \
-		((std::vector<T> *)(storage))->swap(*swap_storage); \
-		data->convertible = storage; \
-	} \
-};
-
-#define numpy_vector_infrastructure(T, name, conv) \
-template <> \
-boost::shared_ptr<std::vector<T> > \
-container_from_object(boost::python::object v) \
-{ \
-	return numpy_container_from_object<std::vector<T> >(v); \
-} \
-static int \
-vector_getbuffer_##name(PyObject *obj, Py_buffer *view, int flags) \
-{ \
-	return pyvector_getbuffer<T>(obj, view, flags, conv); \
-} \
-static PyBufferProcs vec_bufferprocs_##name; \
-numpy_vector_struct(T, name)
-
-#if PY_MAJOR_VERSION < 3
-#define numpy_vector_of(T, name, desc) \
-{ \
-	numpy_vector_from_python_##name(); \
-	boost::python::object cls = register_vector_of<T>(desc); \
-	PyTypeObject *vdclass = (PyTypeObject *)cls.ptr(); \
-	vec_bufferprocs_##name.bf_getbuffer = vector_getbuffer_##name; \
-	vdclass->tp_as_buffer = &vec_bufferprocs_##name; \
-	vdclass->tp_flags |= Py_TPFLAGS_HAVE_NEWBUFFER; \
-}
-#else
-#define numpy_vector_of(T, name, desc) \
-{ \
-	numpy_vector_from_python_##name(); \
-	boost::python::object cls = register_vector_of<T>(desc); \
-	PyTypeObject *vdclass = (PyTypeObject *)cls.ptr(); \
-	vec_bufferprocs_##name.bf_getbuffer = vector_getbuffer_##name; \
-	vdclass->tp_as_buffer = &vec_bufferprocs_##name; \
-}
-#endif
-
-numpy_vector_infrastructure(int64_t, int64_t, "q")
-numpy_vector_infrastructure(uint64_t, uint64_t, "Q")
-numpy_vector_infrastructure(int32_t, int32_t, "i")
-numpy_vector_infrastructure(uint32_t, uint32_t, "I")
-numpy_vector_infrastructure(double, double, "d")
-numpy_vector_infrastructure(float, float, "f")
-
-// Apple, for their own insane reasons, defines uint64_t as
-// "unsigned long long" even on LP64 systems where longs are
-// 64-bit. Because "long long" (not a standard C type!) is not
-// actually the same type as "long", even when both are 64-bit
-// integers, the uint64_t definition above does not do the right
-// thing for size_t on 64-bit Apple systems.
-//
-// Thanks, Apple. "Think Different!"
-#if defined(__APPLE__) && defined(__LP64__)
-numpy_vector_struct(size_t, size_t)
-numpy_vector_struct(ssize_t, ssize_t)
-struct apple_size
-{
-	static PyObject* convert(const std::vector<size_t> &arg) {
-		return boost::python::to_python_value<std::vector<uint64_t> >()(*(std::vector<uint64_t> *)(uintptr_t)(&arg));
-	}
-};
-
-struct apple_ssize
-{
-	static PyObject* convert(const std::vector<ssize_t> &arg) {
-		return boost::python::to_python_value<std::vector<int64_t> >()(*(std::vector<int64_t> *)(intptr_t)(&arg));
-	}
-};
-#endif
-
-template <> boost::shared_ptr<std::vector<std::complex<float> > >
-numpy_container_from_object(boost::python::object v)
-{
-	return complex_numpy_container_from_object<std::vector<std::complex<float> > >(v);
-}
-template <> boost::shared_ptr<std::vector<std::complex<double> > >
-numpy_container_from_object(boost::python::object v)
-{
-	return complex_numpy_container_from_object<std::vector<std::complex<double> > >(v);
-}
-
-numpy_vector_infrastructure(std::complex<double>, cxdouble, "Zd");
-numpy_vector_infrastructure(std::complex<float>, cxfloat, "Zf");
-
 SPT3G_PYTHON_MODULE(core)
 {
 	bp::docstring_options docopts(true, true, false);
@@ -268,28 +150,6 @@ SPT3G_PYTHON_MODULE(core)
 	    "Mathematical and physical constants in G3Units.",
 	    bp::no_init)
 	      BOOST_PP_SEQ_FOR_EACH(G3_CONSTANTS_DEF,~,CONSTANTS);
-
-	// Some POD types
-	register_vector_of<bool>("Bool");
-	numpy_vector_of(int64_t, int64_t, "Int64");
-	numpy_vector_of(uint64_t, uint64_t, "UInt64");
-	numpy_vector_of(int32_t, int32_t, "Int");
-	numpy_vector_of(uint32_t, uint32_t, "UInt");
-
-#if defined(__APPLE__) && defined(__LP64__)
-	numpy_vector_from_python_size_t();
-	numpy_vector_from_python_ssize_t();
-	bp::to_python_converter<std::vector<size_t>, apple_size, false>();
-	bp::to_python_converter<std::vector<ssize_t>, apple_ssize, false>();
-#endif
-
-	numpy_vector_of(double, double, "Double");
-	numpy_vector_of(std::complex<double>, cxdouble, "ComplexDouble");
-	numpy_vector_of(float, float, "Float");
-	numpy_vector_of(std::complex<float>, cxfloat, "ComplexFloat");
-	register_vector_of<std::string>("String");
-	register_vector_of<unsigned char>("UnsignedChar");
-	register_vector_of<G3Time>("G3Time");
 
 	// Do everything else
 	G3ModuleRegistrator::CallRegistrarsFor("core");
