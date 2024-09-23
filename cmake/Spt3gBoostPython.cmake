147cef449e70770e044a33e3b01f25c6d9f74894
Sasha Rahlin <arahlin@uchicago.edu>
2024-09-23 15:18:10 -0500

    simplify python version handling

--- cmake/Spt3gBoostPython.cmake
+++ cmake/Spt3gBoostPython.cmake
@@ -11,24 +11,7 @@ endif()
 set(Boost_USE_STATIC_LIBS OFF)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
+set(Boost_PYTHON_VERSION ${Python_VERSION})
 
-if(NOT DEFINED Boost_PYTHON_TYPE)
-	set(Boost_PYTHON_TYPE python)
-
-	# Hack for some old Boost CMake modules
-	set(_Boost_PYTHON${Python_VERSION_MAJOR}_HEADERS "boost/python.hpp")
-	set(_Boost_PYTHON${Python_VERSION_MAJOR}${Python_VERSION_MINOR}_HEADERS "boost/python.hpp")
-
-	find_package(Boost QUIET COMPONENTS python${Python_VERSION_MAJOR}${Python_VERSION_MINOR})
-	if (${Boost_PYTHON${Python_VERSION_MAJOR}${Python_VERSION_MINOR}_FOUND})
-		set(Boost_PYTHON_TYPE python${Python_VERSION_MAJOR}${Python_VERSION_MINOR})
-	else()
-		find_package(Boost QUIET COMPONENTS python${Python_VERSION_MAJOR})
-		if (${Boost_PYTHON${Python_VERSION_MAJOR}_FOUND})
-			set(Boost_PYTHON_TYPE python${Python_VERSION_MAJOR})
-		endif()
-	endif()
-endif()
-
-find_package(Boost COMPONENTS system iostreams filesystem ${Boost_PYTHON_TYPE} REQUIRED)
+find_package(Boost COMPONENTS system iostreams filesystem python REQUIRED)
 message(STATUS "Found Boost: ${Boost_INCLUDE_DIRS} (found version \"${Boost_VERSION}\")")
