be84953dea14e1db37afd176c1bc14361993fd29
Sasha Rahlin <arahlin@uchicago.edu>
2024-10-22 00:09:20 -0500

    fill in more of the numpy array ufunc interface

--- core/python/quatextensions.py
+++ core/python/quatextensions.py
@@ -3,9 +3,23 @@ from . import Quat, G3VectorQuat, G3TimestreamQuat
 
 __all__ = []
 
+quat_types = (Quat, G3VectorQuat, G3TimestreamQuat)
+
 
 def quat_ufunc(self, ufunc, method, *inputs, **kwargs):
     """Numpy ufunc interface for vectorized quaternion methods."""
+    if ufunc.__name__ in ["isinf", "isnan", "isfinite"] and len(inputs) == 1:
+        v = getattr(ufunc, method)(np.asarray(inputs[0]), **kwargs)
+        if ufunc.__name__ == "isfinite":
+            return np.all(v, axis=-1)
+        return np.any(v, axis=-1)
+    if ufunc.__name__.startswith("logical"):
+        args = []
+        for arg in inputs:
+            if isinstance(arg, quat_types):
+                arg = np.any(np.asarray(arg), axis=-1)
+            args.append(arg)
+        return getattr(ufunc, method)(*args, **kwargs)
     if method != "__call__" or kwargs:
         return NotImplemented
     if len(inputs) == 1:
