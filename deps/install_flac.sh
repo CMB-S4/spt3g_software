1a4f4b40a69d0061b1e446303f9075b5be16e220
Theodore Kisner <tskisner.public@gmail.com>
2025-02-13 20:32:37 -0800

    Try installing flac on macos and avoiding homebrew

---
+++ deps/install_flac.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+# Location of this script
+pushd $(dirname $0) >/dev/null 2>&1
+scriptdir=$(pwd)
+popd >/dev/null 2>&1
+echo "Wheel script directory = ${scriptdir}"
+
+if [ -n "$1" ]; then
+    PREFIX=$1
+else
+    PREFIX=${scriptdir}
+    cd ${PREFIX}
+fi
+
+flac_version=1.4.3
+flac_dir=flac-${flac_version}
+flac_pkg=${flac_dir}.tar.gz
+
+echo "Fetching libFLAC..."
+
+if [ ! -e ${flac_pkg} ]; then
+    curl -SL "https://github.com/xiph/flac/archive/refs/tags/${flac_version}.tar.gz" -o "${flac_pkg}"
+fi
+
+echo "Building libFLAC..."
+
+rm -rf ${flac_dir}
+tar xzf ${flac_pkg} \
+    && pushd ${flac_dir} >/dev/null 2>&1 \
+    && mkdir -p build \
+    && pushd build >/dev/null 2>&1 \
+    && cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER="${CC}" \
+    -DCMAKE_C_FLAGS="${CFLAGS}" \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DBUILD_DOCS=OFF \
+    -DWITH_OGG=OFF \
+    -DBUILD_CXXLIBS=OFF \
+    -DBUILD_PROGRAMS=OFF \
+    -DBUILD_UTILS=OFF \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DINSTALL_MANPAGES=OFF \
+    -DENABLE_MULTITHREADING=ON \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    .. \
+    && make -j 4 install \
+    && popd >/dev/null 2>&1 \
+    && popd >/dev/null 2>&1
