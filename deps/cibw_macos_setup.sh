b4e32a213323678ddbafbc86634ea25cfcb87af3
Theodore Kisner <tskisner.public@gmail.com>
2025-02-18 19:43:12 -0800

    Try ensuring that correct python3-config is linked into the cibw venv

--- deps/cibw_macos_setup.sh
+++ deps/cibw_macos_setup.sh
@@ -15,12 +15,7 @@ echo "Removing files..."
 test ! -d /usr/local || /usr/bin/sudo /usr/bin/find /usr/local -mindepth 1 -maxdepth 1 -type f -print -delete
 test ! -d /opt/homebrew || /usr/bin/sudo /usr/bin/find /opt/homebrew -mindepth 1 -maxdepth 1 -type f -print -delete
 
-# Wipe all python3 frameworks, to avoid conflicts with the one that cibuildwheel
-# is going to install.
-rm -rf /Library/Frameworks/Python.framework/Versions/*
-rm -rf /Library/Developer/CommandLineTools/Library/Frameworks/Python*
-
 # Rehash to forget about the deleted files
 hash -r
 
-echo "===> END DEBUG:  python3:  $(which python3)"
+echo "===> END DEBUG:  python3:  $(which python3)  python3-config:  $(which python3-config)"
