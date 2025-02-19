bd32692ffd03bce7f5e5236963972711eadb4a25
Theodore Kisner <tskisner.public@gmail.com>
2025-02-18 20:52:39 -0800

    Remove debugging.  Add python 3.13 to build matrix.  Attempt to leave homebrew in place.

--- deps/cibw_macos_setup.sh
+++ deps/cibw_macos_setup.sh
@@ -2,20 +2,17 @@
 # runners to move homebrew out of the way:
 # https://github.com/macports/macports-ports/blob/master/.github/workflows/bootstrap.sh
 
-echo "===> START DEBUG:  python3:  $(which python3)"
+# # Move directories to /opt/*-off
+# echo "Moving directories..."
+# sudo mkdir /opt/local-off /opt/homebrew-off
+# test ! -d /usr/local || /usr/bin/sudo /usr/bin/find /usr/local -mindepth 1 -maxdepth 1 -type d -print -exec /bin/mv {} /opt/local-off/ \;
+# test ! -d /opt/homebrew || /usr/bin/sudo /usr/bin/find /opt/homebrew -mindepth 1 -maxdepth 1 -type d -print -exec /bin/mv {} /opt/homebrew-off/ \;
 
-# Move directories to /opt/*-off
-echo "Moving directories..."
-sudo mkdir /opt/local-off /opt/homebrew-off
-test ! -d /usr/local || /usr/bin/sudo /usr/bin/find /usr/local -mindepth 1 -maxdepth 1 -type d -print -exec /bin/mv {} /opt/local-off/ \;
-test ! -d /opt/homebrew || /usr/bin/sudo /usr/bin/find /opt/homebrew -mindepth 1 -maxdepth 1 -type d -print -exec /bin/mv {} /opt/homebrew-off/ \;
+# # Unlink files
+# echo "Removing files..."
+# test ! -d /usr/local || /usr/bin/sudo /usr/bin/find /usr/local -mindepth 1 -maxdepth 1 -type f -print -delete
+# test ! -d /opt/homebrew || /usr/bin/sudo /usr/bin/find /opt/homebrew -mindepth 1 -maxdepth 1 -type f -print -delete
 
-# Unlink files
-echo "Removing files..."
-test ! -d /usr/local || /usr/bin/sudo /usr/bin/find /usr/local -mindepth 1 -maxdepth 1 -type f -print -delete
-test ! -d /opt/homebrew || /usr/bin/sudo /usr/bin/find /opt/homebrew -mindepth 1 -maxdepth 1 -type f -print -delete
+# # Rehash to forget about the deleted files
+# hash -r
 
-# Rehash to forget about the deleted files
-hash -r
-
-echo "===> END DEBUG:  python3:  $(which python3)  python3-config:  $(which python3-config)"
