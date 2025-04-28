04a350b8d58b4d86d32917efce5413f297c9c67d
Sasha Rahlin <arahlin@uchicago.edu>
2025-04-28 12:11:02 -0500

    Use cmake-provided FindGSL, consistent variable names

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -1,14 +1,14 @@
 # - Find FFTW
 # Find the native FFTW includes and library
 #
-# FFTW_INCLUDES - where to find fftw3.h
+# FFTW_INCLUDE_DIRS - where to find fftw3.h
 # FFTW_LIBRARIES - List of libraries when using FFTW.
 # FFTW_FOUND - True if FFTW found.
-if (FFTW_INCLUDES)
+if (FFTW_INCLUDE_DIRS)
 # Already in cache, be silent
 set (FFTW_FIND_QUIETLY TRUE)
-endif (FFTW_INCLUDES)
-find_path (FFTW_INCLUDES fftw3.h HINTS ENV FFTW_INC)
+endif (FFTW_INCLUDE_DIRS)
+find_path (FFTW_INCLUDE_DIRS fftw3.h HINTS ENV FFTW_INC)
 find_library (FFTW_LIBRARIES NAMES fftw3 HINTS ENV FFTW_DIR)
 
 
@@ -22,5 +22,5 @@ mark_as_advanced(FFTW_THREADS_LIBRARY)
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
-find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
-mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDES)
+find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDE_DIRS)
+mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDE_DIRS)
