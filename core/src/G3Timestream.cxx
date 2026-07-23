6a2a9b411cc4d8e7584c6109c4512a2931e007ea
Sasha Rahlin <arahlin@uchicago.edu>
2026-07-23 15:27:01 -0500

    Test that FLAC encoding is ignored for timestreams

--- core/src/G3Timestream.cxx
+++ core/src/G3Timestream.cxx
@@ -110,7 +110,7 @@ template <class A> void G3Timestream::save(A &ar, unsigned v) const
 	uint8_t use_flac = use_flac_;
 	if constexpr (std::is_same_v<A, cereal::JSONOutputArchive>) {
 		if (use_flac_) {
-			log_warn("FLAC encoding not supported for JSON serialization");
+			log_warn("FLAC encoding not supported for JSON serialization; disabling.");
 			use_flac = 0;
 		}
 	}
--- core/tests/jsonio.py
+++ core/tests/jsonio.py
@@ -6,6 +6,9 @@ f = core.G3Frame(core.G3FrameType.Scan)
 f["Five"] = 5
 f["Fives"] = core.G3VectorInt([5, 5, 5])
 f["FiveMap"] = core.G3MapInt({"five": 5})
+ts = core.G3Timestream([5, 5, 5])
+ts.SetFLACCompression(True)
+f["FiveTS"] = ts
 
 json = f.to_json()
 f2 = core.G3Frame.from_json(json)
@@ -16,4 +19,5 @@ for k in f:
     assert type(f[k]) == type(f2[k])
 assert f["Five"] == f2["Five"]
 assert (f["Fives"] == f2["Fives"]).all()
+assert (f["FiveTS"] == f2["FiveTS"]).all()
 assert list(f["FiveMap"].items()) == list(f2["FiveMap"].items())
